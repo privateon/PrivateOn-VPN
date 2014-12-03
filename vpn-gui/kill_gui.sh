f615006ac8f2cf8aaf579e510063daac503406df
Kimmo R. M. Hovi <kimmo@fairwarning.fi>
2014-12-03 22:15:34 +0200

    Add killing the gui to sudo commands

--- vpn-gui/kill_gui.sh
+++ vpn-gui/kill_gui.sh
@@ -2,15 +2,14 @@
 #
 # PrivateOn-VPN -- Because privacy matters.
 #
-# Author: Mikko Rautiainen <info@tietosuojakone.fi>
-#         Kimmo R. M. Hovi <kimmo@fairwarning.fi>
+# Author: Kimmo R. M. Hovi <kimmo@fairwarning.fi>
 #
 # Copyright (C) 2014  PrivateOn / Tietosuojakone Oy, Helsinki, Finland
 # All rights reserved. Use is subject to license terms.
 #
 
 #
-#  vpn-gui/gui.sh     This script closes other vpn-gui instances and starts a new instance
+#  vpn-gui/kill_gui.sh     This script kills an existing instance of the vpn gui
 #
 #   Note: This script must be run with root credentials, preferably using sudo.
 #
@@ -19,11 +18,5 @@ DAEMON=/opt/PrivateOn-VPN/vpn-gui/vpn_gui.pl
 
 PID=`ps aux | grep $DAEMON | grep -v grep | awk 'NR<2 {print $2}'`
 if [ ! -z "$PID" ]; then
-	sudo /opt/PrivateOn-VPN/vpn-gui/kill_gui.sh
+	sudo /usr/bin/kill -9 $PID
 fi
-
-export DISPLAY=:0.0
-export XAUTHORITY=$HOME/.Xauthority
-xhost SI:localuser:root
-
-sudo DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY $DAEMON 1 
